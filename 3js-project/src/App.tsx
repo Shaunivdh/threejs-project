e43818c2cb1cbb9d80a2e2edd38898a3d6e11eab
Shaunivdh <shaunivanderhorst@gmail.com>
2025-09-28 20:46:12 +0100

    Adding environment

--- 3js-project/src/App.tsx
+++ 3js-project/src/App.tsx
@@ -1,15 +1,15 @@
 import { Canvas } from '@react-three/fiber'
 import { Suspense } from 'react'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Environment } from '@react-three/drei' 
 import Platform from './Platform'
-import './app.css' 
+import './app.css'
 
 export default function App() {
   return (
     <div className="scene-wrap">
       <Canvas
-        gl={{ antialias: true, alpha: true }} 
-        style={{ background: 'transparent' }} 
+        gl={{ antialias: true, alpha: true }}
+        style={{ background: 'transparent' }}
         camera={{ position: [10, 10, 10], fov: 50 }}
         onCreated={(state) => {
           state.scene.background = null
@@ -17,9 +17,13 @@ export default function App() {
           state.gl.setClearColor(0x000000, 0)
         }}
       >
-   
+    
         <ambientLight intensity={0.6} />
         <directionalLight position={[5, 10, 5]} intensity={1} />
+
+
+        <Environment preset="park" />
+        {/* options: "sunset", "city", "dawn", "forest", "warehouse", "apartment", "studio", "park", "lobby" */}
 
         <Suspense fallback={null}>
           <Platform />
