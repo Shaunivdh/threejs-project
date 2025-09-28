44896a64b19f302e8bcd2a6837d20eb2f1d0156a
Shaunivdh <shaunivanderhorst@gmail.com>
2025-09-28 19:13:05 +0100

    Extra helper and set up basic block

--- 3js-project/src/App.tsx
+++ 3js-project/src/App.tsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Canvas, useFrame } from '@react-three/fiber'
+import { useRef } from 'react'
+import * as THREE from 'three'
 
-function App() {
-  const [count, setCount] = useState(0)
+function Box() {
+  const ref = useRef<THREE.Mesh>(null!)
+  useFrame(() => {
+    ref.current.rotation.x += 0.01
+    ref.current.rotation.y += 0.01
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <mesh ref={ref}>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="#77c36b" />
+    </mesh>
   )
 }
 
-export default App
+export default function App() {
+  return (
+    <div style={{ width: '100%', height: '100vh' }}>
+      <Canvas
+        camera={{ position: [3, 3, 3], fov: 60 }}
+        onCreated={({ gl }) => gl.setClearColor(new THREE.Color('#202020'), 1)}
+      >
+        <ambientLight intensity={0.6} />
+        <directionalLight position={[5, 5, 5]} intensity={1} />
+        <Box />
+      </Canvas>
+    </div>
+  )
+}
