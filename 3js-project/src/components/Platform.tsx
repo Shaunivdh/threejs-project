87f040db97031ec24c9ea0a92bc1e7d548e86624
Shaunivdh <shaunivanderhorst@gmail.com>
2025-12-15 11:11:17 +0000

    Making platform larger

--- 3js-project/src/components/Platform.tsx
+++ 3js-project/src/components/Platform.tsx
@@ -3,42 +3,41 @@ import * as THREE from "three";
 import { useMemo } from "react";
 
 export default function Platform(props: JSX.IntrinsicElements["group"]) {
-  const shape = useMemo(() => {
-    const s = new THREE.Shape();
-    const width = 9.5,
-      height = 8,
-      radius = 0.5;
-    s.moveTo(-width / 2 + radius, height / 2);
-    s.lineTo(width / 2 - radius, height / 2);
-    s.quadraticCurveTo(width / 2, height / 2, width / 2, height / 2 - radius);
-    s.lineTo(width / 2, -height / 2 + radius);
-    s.quadraticCurveTo(width / 2, -height / 2, width / 2 - radius, -height / 2);
-    s.lineTo(-width / 2 + radius, -height / 2);
-    s.quadraticCurveTo(
-      -width / 2,
-      -height / 2,
-      -width / 2,
-      -height / 2 + radius
-    );
-    s.lineTo(-width / 2, height / 2 - radius);
-    s.quadraticCurveTo(-width / 2, height / 2, -width / 2 + radius, height / 2);
-    return s;
-  }, []);
+  const size = 150;
+  const yOffset = -0.2;
 
-  const depth = 0.2;
+  const alphaMap = useMemo(() => {
+    const canvas = document.createElement("canvas");
+    canvas.width = canvas.height = 256;
+
+    const ctx = canvas.getContext("2d")!;
+    const gradient = ctx.createRadialGradient(128, 128, 60, 128, 128, 128);
+
+    gradient.addColorStop(0, "rgba(255,255,255,1)");
+    gradient.addColorStop(1, "rgba(255,255,255,0)");
+
+    ctx.fillStyle = gradient;
+    ctx.fillRect(0, 0, 256, 256);
+
+    const texture = new THREE.CanvasTexture(canvas);
+    texture.wrapS = texture.wrapT = THREE.ClampToEdgeWrapping;
+    return texture;
+  }, []);
 
   return (
     <group {...props}>
       <mesh
-        position={[0, -depth, 0]}
+        position={[0, yOffset, 0]}
         rotation={[-Math.PI / 2, 0, 0]}
         receiveShadow
       >
-        <extrudeGeometry args={[shape, { depth, bevelEnabled: false }]} />
+        <planeGeometry args={[size, size]} />
         <meshStandardMaterial
           color="#8fdc8a"
           roughness={0.9}
           metalness={0}
+          transparent
+          alphaMap={alphaMap}
           envMapIntensity={0.5}
           toneMapped={false}
         />
